3baff1feb811ff43bd15178be1a51e3abf84d608
lzalom <lubos.zalom@gmail.com>
2014-07-15 14:02:05 +0200

    produkce

---
+++ js/init.js
@@ -0,0 +1,183 @@
+$(document).ready(function(){
+     
+    /* init*/
+    var numTender;
+    var numSwollen;
+    var mode = 'das28-3';
+    var modeTxt = 'DAS28';
+    $('.formSectionCalculate h2').text(modeTxt);
+    var result;
+    var tenderJointScore = 0;
+    var swollenJointScore = 0;
+    $('input[type=text]').each(function(){
+        $(this).val('');
+    }); 
+    $('input[name=health]').val(0); 
+    $('input[type=radio], input[type=checkbox]').each(function(){
+        $(this).removeAttr('checked');
+    }); 
+    $('input#mannequin, input#esr, input#global').each(function(){
+        $( this ).prop('checked', true);
+    }); 
+     
+    $( "#slider" ).slider({
+        range: "min",
+        value: 0,
+        min: 0,
+        max: 100,
+        slide: function( event, ui ) {
+        $( "input[name=health]" ).val( ui.value );
+        }
+    });
+    $("input[name=health]").keyup(function(){
+        $( "#slider" ).slider( "value", $(this).val() );
+    });
+    
+    /**/
+    $('input[name=enter]').change(function(){
+        if ( $(this).val()=='type' ){
+            $('input#tender, input#swollen').removeAttr('disabled').removeClass('disabled');
+            $('.overlay').removeClass('hidden');
+            $('.col.colRight input').removeAttr('checked');
+        }else{
+            $('input#tender, input#swollen').attr('disabled','disabled').addClass('disabled').val('');
+            $('.overlay').addClass('hidden');
+        }
+    });
+    
+    /**/
+    $('input[name=measures]').change(function(){
+        if ( $(this).val()=='esr' ){
+            $('input#crpText').attr('disabled','disabled').addClass('disabled').val('');
+            $('input#esrText').removeAttr('disabled').removeClass('disabled');
+            if ( $('input[name=global]').is(':checked') ){
+                mode = 'das28';
+                modeTxt = 'DAS28';              
+            }else{
+                mode = 'das28-3';
+                modeTxt = 'DAS28 3';            
+            }
+            $('.formSectionCalculate h2').text(modeTxt);
+        }else{
+            $('input#esrText').attr('disabled','disabled').addClass('disabled').val('');
+            $('input#crpText').removeAttr('disabled').removeClass('disabled');
+            if ( $('input[name=global]').is(':checked') ){
+                mode = 'das28-crp';
+                modeTxt = 'DAS28-CRP';               
+            }else{
+                mode = 'das28-crp-3';
+                modeTxt = 'DAS28-CRP 3';             
+            }
+            $('.formSectionCalculate h2').text(modeTxt);
+        }
+    });
+    
+    /**/
+    $('input[name=global]').change(function(){
+        if ( $(this).is(':checked')==true ){
+            $('#slider, .left, .right').removeClass('hidden');
+            $('input[name=health]').removeClass('disabled').removeAttr('disabled');
+            $('input[name=health]').val(0); 
+            if ( $('input#esr').is(':checked') ){
+                mode = 'das28';
+                modeTxt = 'DAS28';              
+            }else{
+                mode = 'das28-crp';
+                modeTxt = 'DAS28-CRP';            
+            }
+            $('.formSectionCalculate h2').text(modeTxt);
+        }else{
+            $('#slider, .left, .right').addClass('hidden');
+            $('input[name=health]').val('').addClass('disabled').attr('disabled','disabled');
+            $( "#slider" ).slider( "value", 0 );
+            if ( $('input#esr').is(':checked') ){
+                mode = 'das28-3';
+                modeTxt = 'DAS28 3';               
+            }else{
+                mode = 'das28-crp-3';
+                modeTxt = 'DAS28-CRP 3';            
+            }
+            $('.formSectionCalculate h2').text(modeTxt);
+        }
+    });
+    
+    /**/
+    $('.clear').click(function(e){
+        $(this).closest('.mannequinBox').find('input').removeAttr('checked');
+        tenderJointScore = $('.mannequinLeft input:checked').length;
+        swollenJointScore = $('.mannequinRight input:checked').length;
+        $('input#tender').val(tenderJointScore);
+        $('input#swollen').val(swollenJointScore);
+        e.preventDefault();
+    });
+    
+    /**/
+    $('.mannequinBox input').change(function(){
+        tenderJointScore = $('.mannequinLeft input:checked').length;
+        swollenJointScore = $('.mannequinRight input:checked').length;
+        $('input#tender').val(tenderJointScore);
+        $('input#swollen').val(swollenJointScore);
+    });
+    
+    /**/
+    $('button[name=calculate]').click(function(e){
+        if ((mode=='das28-3')&&( $('input[name=global]').is(':checked')==true )){
+            result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( $('input#esrText').val() )) + 0.014 * $('input[name=health]').val();
+        
+        }
+        if ((mode=='das28-3')&&( $('input[name=global]').is(':checked')==false )){
+            result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( $('input#esrText').val() )) *1.08 + 0.16;
+        
+        }
+        if ((mode=='das28-crp-3')&&( $('input[name=global]').is(':checked')==true )){
+            //result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log( $('input#crpText').val()+1)] + 0.014 * $('input[name=health]').val() + 0.96;
+            // nResult = 0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(CRPScore+1) + 0.014 * patientGlobalScore + 0.96
+            //
+            result = 0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log($('input#crpText').val()+1) + 0.014 * $('input[name=health]').val() + 0.96;
+        
+        }
+        if ((mode=='das28-crp-3')&&( $('input[name=global]').is(':checked')==false )){
+            //result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log( $('input#crpText').val()+1)] * 1.10 + 1.15;
+            //nResult = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(CRPScore+1)] * 1.10 + 1.15
+            //
+            result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log($('input#crpText').val()+1)] * 1.10 + 1.15;
+        
+        }
+        alert(mode+' '+result);
+        e.preventDefault();
+    });
+    
+    
+});
+
+/*
+
+if (errorText == "")
+        {
+            case "DAS28 3":
+
+              // FORMULA:  DAS28(3) = [0.56*sqrt(t28) + 0.28*sqrt(sw28) + 0.70*Ln(ESR)]*1.08 + 0.16 
+
+              nResult = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log(ESRScore)) *1.08 + 0.16
+              break;
+
+            case "DAS28-CRP 3":
+
+            // FORMULA: DAS28-CRP(3) = [0.56*sqrt(TJC28) + 0.28*sqrt(SJC28) + 0.36*ln(CRP+1)] * 1.10 + 1.15 
+
+              nResult = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(CRPScore+1)] * 1.10 + 1.15
+              break;
+            default:
+             errorText = "Unknown test";
+             alert("Error Text: " + errorText)
+            }    
+            nResult = Math.round(nResult * 100) / 100 //round to two decimal places
+        }
+    }
+
+*/
+
+$(window).ready(function(){
+
+ 
+});
