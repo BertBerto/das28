50ddb18bb5ba11169a014d89371abd95ed5d5053
Lubos Zalom <lubos.zalom@gmail.com>
2014-07-18 13:00:20 +0200

    produkce

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     /* init*/
     var numTender;
     var numSwollen;
-    var mode = 'das28-3';
+    var mode = 'das28';
     var modeTxt = 'DAS28';
     $('.formSectionCalculate h2').text(modeTxt);
     var result;
@@ -76,29 +76,35 @@ $(document).ready(function(){
     });
     
     /* validation */
-    $('input[type=text]').keyup(function(){
+    $('input[type=text][name!=patient-id]').keyup(function(){
             var control = isNaN( $(this).val() );
             if (control==true){
                 alert('Je třeba zadat číslo');
-                $(this).val('').focus();
+                if (( $(this).is('#tender')==true )||( $(this).is('#swollen')==true )){
+                    $(this).val(0).focus();
+                }else if ( $(this).is('#health') ){
+                    $(this).val(0).focus();
+                }else{
+                    $(this).val(1).focus();
+                }
             }    
     });
     $('input#tender, input#swollen').keyup(function(){
             if ( $(this).val()>28){
                 alert('Je třeba zadat číslo od 0 - 28');
-                $(this).val('').focus();
+                $(this).val(0).focus();
             }    
     });
     $('input#esrText, input#crpText').keyup(function(){
             if ( $(this).val()>150){
-                alert('Je třeba zadat číslo od 0 - 150');
-                $(this).val('').focus();
+                alert('Je třeba zadat číslo od 1 - 150');
+                $(this).val(1).focus();
             }    
     });
     $('input[name=health]').keyup(function(){
             if ( $(this).val()>100){
                 alert('Je třeba zadat číslo od 0 - 100');
-                $(this).val('').focus();
+                $(this).val(0).focus();
             }    
     });
     
@@ -131,14 +137,22 @@ $(document).ready(function(){
         }
     });
     
-    /**/
+    /* clear results */
     $('.clear').click(function(e){
         $(this).closest('.mannequinBox').find('input').removeAttr('checked');
         tenderJointScore = $('.mannequinLeft input:checked').length;
         swollenJointScore = $('.mannequinRight input:checked').length;
         $('input#tender').val(tenderJointScore);
         $('input#swollen').val(swollenJointScore);
+        $('.result .txt, .result .num').text('');
+        $('.formula p').text('Formula:');
+        $('.result').removeClass('RemissionStatus').removeClass('LowActivityStatus').removeClass('ModerateActivityStatus').removeClass('HighActivityStatus');
         e.preventDefault();
+    });
+    $('input').change(function(){
+        $('.result .txt, .result .num').text('');
+        $('.formula p').text('Formula:');
+        $('.result').removeClass('RemissionStatus').removeClass('LowActivityStatus').removeClass('ModerateActivityStatus').removeClass('HighActivityStatus');
     });
     
     /**/
@@ -151,31 +165,70 @@ $(document).ready(function(){
     
     /**/
     $('button[name=calculate]').click(function(e){
-        if (mode=='das28'){
-            result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( $('input#esrText').val() )) + 0.014 * $('input[name=health]').val();
-            formula = '(0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.70 * Math.log( '+$('input#esrText').val()+' )) + 0.014 * '+$('input[name=health]').val();
+        var result = 0;
+        var resultText;
+        var formula = '';
+        var resultClass;
+        var esrScore;
+        var crpScore;
+        if ( $('input#esrText').val()=='' ){
+            esrScore = 0;
+        }else{
+            esrScore = parseInt( $('input#esrText').val() );
         }
-        if (mode=='das28-3'){
-            result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( $('input#esrText').val() )) *1.08 + 0.16;
-            formula = '(0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.70 * Math.log( '+$('input#esrText').val()+' )) *1.08 + 0.16';
-        
+        if ( $('input#crpText').val()=='' ){
+            crpScore = 0;
+        }else{
+            crpScore = parseInt( $('input#crpText').val() );
         }
-        if (mode=='das28-crp-3'){
-            //result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log( $('input#crpText').val()+1)] * 1.10 + 1.15;
-            //nResult = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(CRPScore+1)] * 1.10 + 1.15
-            //
-            result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log($('input#crpText').val()+1)] * 1.10 + 1.15;
-            formula = '[0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.36 * Math.log( '+$('input#crpText').val()+' +1)] * 1.10 + 1.15';
+        if (esrScore>0){
+             if (mode=='das28'){
+                result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( esrScore )) + 0.014 * $('input[name=health]').val();
+                formula = '(0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.70 * Math.log( '+$('input#esrText').val()+' )) + 0.014 * '+$('input[name=health]').val();
+            }
+            if (mode=='das28-3'){
+                result = (0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.70 * Math.log( esrScore )) *1.08 + 0.16;
+                formula = '(0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.70 * Math.log( '+$('input#esrText').val()+' )) *1.08 + 0.16';
+            
+            }           
         }
-        if (mode=='das28-crp'){
-            //result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log( $('input#crpText').val()+1)] + 0.014 * $('input[name=health]').val() + 0.96;
-            // nResult = 0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(CRPScore+1) + 0.014 * patientGlobalScore + 0.96
-            //
-            result = 0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log($('input#crpText').val()+1) + 0.014 * $('input[name=health]').val() + 0.96;
-            formula = '0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.36 * Math.log( '+$('input#crpText').val()+' +1) + 0.014 * '+$('input[name=health]').val()+' + 0.96';
+        if (crpScore>0){
+              if (mode=='das28-crp-3'){
+                result = [0.56 * Math.sqrt(tenderJointScore) + 0.28 * Math.sqrt(swollenJointScore) + 0.36 * Math.log(crpScore+1)] * 1.10 + 1.15;
+                formula = '[0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.36 * Math.log( '+$('input#crpText').val()+' +1)] * 1.10 + 1.15';
+            }
+            if (mode=='das28-crp'){
+                var result = (0.56 * Math.sqrt(tenderJointScore)) + (0.28 * Math.sqrt(swollenJointScore)) + (0.36 * Math.log( (crpScore+1) )) + ((0.014 * $('input[name=health]').val()) + 0.96);
+                formula = '0.56 * Math.sqrt('+tenderJointScore+') + 0.28 * Math.sqrt('+swollenJointScore+') + 0.36 * Math.log('+$('input#crpText').val()+' +1) + 0.014 * '+$('input[name=health]').val()+' + 0.96';
+            }          
         }
-        $('.result p').html(result);
-        $('.formula p').html(formula);
+
+        result = Math.round(result * 100) / 100;
+        if (result < 2.6)
+        {
+            resultText = "Remission";
+            resultClass = "RemissionStatus";
+        }
+        else if (result < 3.2)
+        {
+            resultText = "Low disease activity";
+            resultClass = "LowActivityStatus";
+        }
+        else if (result >=3.2 && result <= 5.1)
+        {
+            resultText = "Moderate disease activity";
+            resultClass = "ModerateActivityStatus";
+        }  
+        else if (result >5.1)
+        {
+            resultText = "High disease activity";
+            resultClass = "HighActivityStatus";
+        } 
+        $('.result').addClass(resultClass);
+        $('.result p.num').html(result);
+        $('.result p.txt').html(resultText);
+        $('.formula p').html('Formula: '+formula);     
+
         e.preventDefault();
     });
     
@@ -184,6 +237,25 @@ $(document).ready(function(){
 
 /*
 
+FORMULA:DAS28-CRP(4) = 0.56*sqrt(TJC28) + 0.28*sqrt(SJC28) + 0.36*ln(CRP+1) + 0.014*GH + 0.96     Reference: http://www.das-score.nl
+
+
+2 crp 1 0
+0.56 * Math.sqrt(2) + 0.28 * Math.sqrt(0) + 0.36 * Math.log( 1 +1) + 0.014 * 0 + 0.96
+má vyjít 2
+ale vychází 2,6
+
+0.56 X 1,41 + 0.28 X 0 + 0.36 X 0,69 + 0 + 0,96
+0.78 + 0 + 0,24 + 0,96
+
+
+
+
+
+
+
+
+ 
 if (errorText == "")
         {
             case "DAS28 3":
